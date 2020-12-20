ec933ce4174763bf2b1cc25e01f1d350f9fdf645
Kichiyaki <dawidwysokinski000@gmail.com>
2020-12-20 14:40:02 +0100

    add ScrollToTop

--- src/features/App.tsx
+++ src/features/App.tsx
@@ -4,6 +4,7 @@ import * as ROUTES from '@config/routes';
 
 import { Switch, Route } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
+import ScrollToTop from '@common/ScrollToTop/ScrollToTop';
 
 import IndexPage from './IndexPage/IndexPage';
 import NotFoundPage from './NotFoundPage/NotFoundPage';
@@ -24,6 +25,7 @@ function App() {
         </Route>
       </Switch>
       <CssBaseline />
+      <ScrollToTop />
     </Fragment>
   );
 }
