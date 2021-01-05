fe550c372f940247de806df94cf2e7e9a63d6bd8
Kichiyaki <dawidwysokinski000@gmail.com>
2021-01-05 17:27:40 +0100

    update DateTimePicker/Date picker styles

--- src/common/Picker/DatePicker.tsx
+++ src/common/Picker/DatePicker.tsx
@@ -1,28 +1,17 @@
 import React from 'react';
-import clsx from 'clsx';
 import {
   DatePicker as MUIDatePicker,
   DatePickerProps,
 } from '@material-ui/pickers';
 import { DATE_FORMAT } from '@config/app';
 import useI18N from './useI18N';
-import useDialogStyles from './useDialogStyles';
 
 function DatePicker({
-  className,
   format = DATE_FORMAT.DAY_MONTH_AND_YEAR,
   ...props
 }: DatePickerProps) {
-  const classes = useDialogStyles();
   const translations = useI18N();
-  return (
-    <MUIDatePicker
-      {...props}
-      format={format}
-      DialogProps={{ className: clsx(className, classes.dialog) }}
-      {...translations}
-    />
-  );
+  return <MUIDatePicker {...props} format={format} {...translations} />;
 }
 
 export default DatePicker;
--- src/common/Picker/DateTimePicker.tsx
+++ src/common/Picker/DateTimePicker.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import clsx from 'clsx';
 import { DATE_FORMAT } from '@config/app';
 import useI18N from './useI18N';
-import useDialogStyles from './useDialogStyles';
 
 import {
   DateTimePicker as MUIDateTimePicker,
@@ -10,19 +8,16 @@ import {
 } from '@material-ui/pickers';
 
 function DateTimePicker({
-  className,
   ampm = false,
   format = DATE_FORMAT.HOUR_MINUTES_DAY_MONTH_AND_YEAR,
   ...props
 }: DateTimePickerProps) {
-  const classes = useDialogStyles();
   const translations = useI18N();
   return (
     <MUIDateTimePicker
       {...props}
       ampm={ampm}
       format={format}
-      DialogProps={{ className: clsx(className, classes.dialog) }}
       {...translations}
     />
   );
